343c36aa73ed788833819b6b0cd2eaf0065361de
mad444b <bokimen30@gmail.com>
2021-07-13 00:51:22 +0200

    Revert "Update index.js"
    This reverts commit fc72f28bee2622ceeabd065b0f9eb3f3c514ae02.

--- src/index.js
+++ src/index.js
@@ -1,8 +1,14 @@
 import { link } from 'fs';
 import React from 'react';
 import ReactDom from 'react-dom';
+<<<<<<< HEAD
 import 'semantic-ui-css/semantic.min.css';
 import Gallery from './gallery';
+=======
+import 'semantic-ui-css/semantic.min.css'
+import Body from './player';
+
+>>>>>>> e83068c2cf36ab72314ef7146a79d71c6e4796bb
 //our page
 
 
@@ -11,8 +17,11 @@ class App extends React.Component {
     render(props) {
         return (
             <div>
+<<<<<<< HEAD
                 <Gallery/>
+=======
                 <Body />
+>>>>>>> e83068c2cf36ab72314ef7146a79d71c6e4796bb
             </div>
         )
     };
